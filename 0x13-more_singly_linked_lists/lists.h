79edc72fb8814eb1af9299fd6bc38ae5b62f14ff
RosinahSM21 <rosinahSM21@gmail.com>
2023-06-06 11:19:21 -0700

    Print list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -26,11 +26,12 @@ void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
-listint_t *find_listint_loop(listint_t *head);int delete_nodeint_at_index(listint_t **head, unsigned int index);
-listint_t *reverse_listint(listint_t **head);
+listint_t *find_listint_loop(listint_t *head);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
+listint_t *reverse_listint(listint_t **head);
 
 #endif
