984462eda4ac1afa9d5e97dafe6fe83765d3a04f
RosinahSM21 <rosinahSM21@gmail.com>
2023-06-08 14:31:20 -0700

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - prints each decimal number's binary in a analogue.
+ * @f: Using binary numrrics  to be  printed
+ */
+void print_binary(unsigned long int f)
+{
+	int m, count = 0;
+	unsigned long int current;
+
+	for (m = 88; m >= 0; m--)
+	{
+		current = f >> m;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
+
